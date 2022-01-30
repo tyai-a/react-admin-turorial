6d24c68be8db4e7693baee5118e0ce10bb8f06fc
tyai-a <achi@amr.co.jp>
2022-01-30 23:59:17 +0900

    日本語化パッケージを追加し、ダッシュボードトップを日本語に

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./Record/UserList";
 import { PostList, PostEdit, PostCreate } from "./Record/PostList";
-import { Top } from "./Page/Top"
-import authProvider from "./Utility/authProvide"
-import PostIcon from '@material-ui/icons/Book';
-import UserIcon from '@material-ui/icons/Group';
+import { Top } from "./Page/Top";
+import authProvider from "./Utility/authProvide";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+import polyglotI18nProvider from "ra-i18n-polyglot";
+import japaneseMessages from "@bicstone/ra-language-japanese";
+
+const i18nProvider = polyglotI18nProvider(() => japaneseMessages, "ja");
+
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
-	<Admin dataProvider={dataProvider} authProvider={authProvider} dashboard={Top} >
+	<Admin
+		dataProvider={dataProvider}
+		authProvider={authProvider}
+		dashboard={Top}
+		i18nProvider={i18nProvider}
+	>
 		<Resource name="users" list={UserList} icon={UserIcon} />
 		<Resource
 			name="posts"
 			list={PostList}
 			edit={PostEdit}
 			create={PostCreate}
-      icon={PostIcon}
+			icon={PostIcon}
 		/>
 	</Admin>
 );
--- src/Page/Top.js
+++ src/Page/Top.js
@@ -2,7 +2,7 @@ import { Card, CardContent, CardHeader } from '@material-ui/core';
 
 export const Top = () => (
     <Card>
-        <CardHeader title="Welcome to the administration" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+        <CardHeader title="ダッシュボード" />
+        <CardContent>サイドバーから閲覧したいページを選択します。</CardContent>
     </Card>
 );
